d17d78a348e8f52d3b74e39cfcb8b09c6b0e61bd
粟夢 <79361022+Awayume@users.noreply.github.com>
2021-04-16 22:07:37 +0900

    Update Updater.sh

--- Updater.sh
+++ Updater.sh
@@ -1,1 +1,18 @@
+#!/bin/sh
 
+# 新バージョンの案内
+echo Ver
+echo アップデートをスタートします。Enterを押してください。
+read Wait
+# 旧バージョンの削除
+cd $home
+rm upload.sh
+# 新バージョンのダウンロード
+wget URL
+wget VCURL
+chmod 755 ./upload.sh
+echo アップデートが完了しました。ツールを起動します。
+echo Enterを押してください。
+read Wait
+./upload.sh
+rm -f ./uploader.sh
