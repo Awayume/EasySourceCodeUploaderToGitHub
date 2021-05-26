c92644ee5c905ee1398ceebe0cae2b39dcbe30cd
粟夢 <79361022+Awayume@users.noreply.github.com>
2021-05-27 01:09:46 +0900

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # 新バージョンの案内
-echo Ver2.1
-echo アップロード準備の動作の改善をしました。
+echo Ver2.2
+echo アップデート時の挙動を変更しました。
+echo 使用するコマンドを変更しました。
 # アップデート開始文
 echo アップデートをスタートします。Enterを押してください。
 read Wait
@@ -10,7 +11,7 @@ read Wait
 cd $home
 rm upload.sh
 # 新バージョンのダウンロード
-wget https://github.com/Awayume/EasySourceCodeUploaderToGitHub/releases/download/v2.1/upload.sh
+wget https://github.com/Awayume/EasySourceCodeUploaderToGitHub/releases/download/v2.2/upload.sh
 chmod 755 upload.sh
 rm -rf EasySourceCodeUploaderToGitHub
 echo アップデートが完了しました。ツールを起動します。
